6bde7914adab6c92a77b0296dadd06aa3bd40399
Miguel <mike.lo.123@hotmail.com>
2020-12-05 12:16:19 +0000

    Add February Page

--- src/components/MonthChart.js
+++ src/components/MonthChart.js
@@ -16,6 +16,15 @@ var januaryEntertainment = 0;
 var januaryCarCosts = 0;
 var januaryOthers = 0;
 
+var februaryRent = 0;
+var februaryBills = 0;
+var februaryGroceries = 0;
+var februaryHolidays = 0;
+var februaryClothes = 0;
+var februaryEntertainment = 0;
+var februaryCarCosts = 0;
+var februaryOthers = 0;
+
 export function JanuaryCategories() {
     records.forEach(item => {
         if ((item.category == 'Rent') && (item.month == 'January')) {
@@ -43,6 +52,55 @@ export function JanuaryCategories() {
         datasets: [
             {
             data: [januaryRent, januaryBills, januaryGroceries, januaryHolidays, januaryClothes, januaryEntertainment, januaryCarCosts, januaryOthers],
+            backgroundColor: [
+                "#003f5c",
+                "#2f4b7c",
+                "#665191",
+                "#a05195",
+                "#d45087",
+                "#f95d6a",
+                "#ff7c43",
+                "#ffa600"
+            ]
+            }
+        ]
+        }
+    }
+
+    return (
+        <MDBContainer >
+            <Pie data={data.dataPie} options={{ responsive: true }}/>
+        </MDBContainer>
+    )
+};
+
+export function FebruaryCategories() {
+    records.forEach(item => {
+        if ((item.category == 'Rent') && (item.month == 'February')) {
+            februaryRent += Number(item.expense);
+        } else if ((item.category == 'Bills') && (item.month == 'February')) {
+            februaryBills += Number(item.expense);
+        } else if ((item.category == 'Groceries') && (item.month == 'February')) {
+            februaryGroceries += Number(item.expense);
+        } else if ((item.category == 'Holidays') && (item.month == 'February')) {
+            februaryHolidays += Number(item.expense);
+        } else if ((item.category == 'Clothes') && (item.month == 'February')) {
+            februaryClothes += Number(item.expense);
+        } else if ((item.category == 'Entertainment') && (item.month == 'February')) {
+            februaryEntertainment += Number(item.expense);
+        } else if ((item.category == 'Car Costs') && (item.month == 'February')) {
+            februaryCarCosts += Number(item.expense);
+        } else if ((item.category == 'Others') && (item.month == 'February')) {
+            februaryOthers += Number(item.expense);
+        }
+    });
+    
+    const data = {
+        dataPie: {
+        labels: ['Rent', 'Bills', 'Groceries', 'Holidays', 'Clothes', 'Entertainment', 'Car Costs', 'Others'],
+        datasets: [
+            {
+            data: [februaryRent, februaryBills, februaryGroceries, februaryHolidays, februaryClothes, februaryEntertainment, februaryCarCosts, februaryOthers],
             backgroundColor: [
                 "#003f5c",
                 "#2f4b7c",
